01384a17aca96dc8c0502d5f4b8d37354a7d6ba5
Kairat-Z <zh.kairat2@gmail.com>
2025-02-21 16:05:41 -0600

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "group5-grafana-practice1245" # Replace with the same value as your s3 bucket on AWS Console
-    key            = "terraform.tfstate"
-    region         = "us-east-2" # Replace with your own value
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
